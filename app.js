af6259f13dcca6cc83482edeee3f5bb244d04f51
Karan Aggarwal <karan.aggarwal@nearbuy.com>
2018-05-25 14:07:47 +0530

    Added WebP Encoder used imagemin-webp

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const upload = multer({ dest: 'upload/'});
 const imagemin = require('imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminGuetzli = require('imagemin-guetzli');
+const imageminWebP = require('imagemin-webp');
 
 var fileUploadName = "";
 
@@ -27,11 +28,11 @@ function removeMetadata(fileName){
 app.post('/upload', upload.single('file'), function (req, res) {
     (async () => {
         uploadAction(req,"mozjpeg");
-        await imagemin(['upload/mozjpeg/*.jpg'], 'build/images/mozjpeg', {
+        imagemin(['upload/mozjpeg/*.jpg'], 'build/images/mozjpeg', {
             use: [
                 imageminMozjpeg()
             ]
-        });
+        })
         console.log('Images optimized');
     })();
 });
@@ -43,31 +44,60 @@ app.post("/uploadG", upload.single('file'), function(req,res){
             use: [
                 imageminGuetzli({quality: 84})
             ]
-        });
+        })
         console.log('Images optimized');
-        })();
+    })();
+});
+
+app.post("/uploadW", upload.single('file'), function(req,res){
+    (async () => {
+        uploadAction(req,"webp");
+        await imagemin(['upload/webp/*.webp'], 'build/images/webp', {
+            use:[
+                imageminWebP({quality:80})
+            ]
+        })
+        console.log('Images optimized');
+    })();
 });
 
 function uploadAction(req,pathname){
+    
     var tempPath = req.file.path;
-    targetPath = path.resolve("./upload/"+pathname+"/image"+req.file.filename+".jpg");
+
+    var path_val = "./upload/"+pathname+"/image"+req.file.filename;
+
+    if(pathname !== "webp"){
+        path_val += ".jpg";
+    }  else {
+        path_val += ".webp";
+    }
+
+    targetPath = path.resolve(path_val);
     if (path.extname(req.file.originalname).toLowerCase() === '.jpg' || 
-        path.extname(req.file.originalname).toLowerCase() === '.png') {
+        path.extname(req.file.originalname).toLowerCase() === '.webp'){
         fs.rename(tempPath, targetPath, function(err) {
-            if (err) throw err;
-            console.log("Upload completed!");
+           if (err) throw err;
+           console.log("Upload completed!");
         });
-        //currently the code will handle only jpg files, needs to be modified for
+
+        //currently the code will handle only jpg and webp files, needs to be modified for
         //other file formats
-        fileUploadName="image"+req.file.filename+".jpg";
+        
+        if(pathname !== 'webp'){
+            fileUploadName="image"+req.file.filename+".jpg";    
+        } else {
+            fileUploadName="image"+req.file.filename+".webp";
+        }
+
     } else {
         fs.unlink(tempPath, function () {
             if (err) throw err;
-            console.error("Only .jpg  and .png files are allowed!");
+            console.error("Only .jpg  and .webp files are allowed!");
         });
     }
 }
 
-app.listen(3030, function() {
+app.listen(3030, function(){
     console.log("Server Started");
 });
